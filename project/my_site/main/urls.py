e32c7698dc7de5c2114cc58d80620410e08c7594
ZoLonOX <miv1748717@gmail.com>
2024-12-23 20:42:17 +0300

    Add files via upload

--- project/my_site/main/urls.py
+++ project/my_site/main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index ),
     path('community', views.community),
     path('create', views.create),
     path('join', views.join),
-    path('join', views.login),
-]
+    path('login', views.login),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
